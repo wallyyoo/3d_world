1ebc8cebf09172e7ba94a8937478664dbb8c6359
wallyyoo <hyase22@naver.com>
2025-05-20 21:25:34 +0900

    edit : 이동로직 + 점프 구현
    점프 파워 조절해야함

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+
+    private void Awake()
+    {
+        {
+            CharacterManager.Instance.Player = this;
+            controller = GetComponent<PlayerController>();
+        }
+    }
+}
---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+   [Header("Movement")] 
+   public float moveSpeed;
+   private Vector2 curMovementInput;
+   public float jumpPower;
+   public LayerMask groundLayerMask;
+
+   [Header("Look")]
+   public Transform cameraContainer;
+   public float minXLook;
+   public float maxXLook;
+   private float camCurXRot;
+   public float lookSensitivity;
+
+   private Vector2 mouseDelta;
+   
+   [HideInInspector]
+   public bool canLook = true;
+   private Rigidbody rb;
+
+   private void Awake()
+   {
+      rb = GetComponent<Rigidbody>();
+   }
+
+   void Start()
+   {
+      Cursor.lockState = CursorLockMode.Locked;
+   }
+   
+   private void FixedUpdate()
+   {
+      Move();
+    
+   }
+
+   private void LateUpdate()
+   {
+      if(canLook)
+      {
+         CameraLook();
+      }
+   }
+
+   public void OnLookInput(InputAction.CallbackContext context)
+   {
+      mouseDelta = context.ReadValue<Vector2>();
+   }
+
+   public void OnMoveInput(InputAction.CallbackContext context)
+   {
+      if (context.phase == InputActionPhase.Performed)
+      {
+         curMovementInput = context.ReadValue<Vector2>();
+      }
+      else if (context.phase == InputActionPhase.Canceled)
+      {
+         curMovementInput = Vector2.zero;
+      }
+   }
+
+
+   public void OnJumpInput(InputAction.CallbackContext context)
+   {
+    //  if (context.phase == InputActionPhase.Started && IsGrounded())
+    if (context.phase == InputActionPhase.Started)
+      {
+         Debug.Log("Jump pressed. Grounded :" +IsGrounded());
+         //rb.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+         if (IsGrounded())
+         {
+            
+        rb.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+            Debug.Log("jump force applied. velocity" + rb.velocity);
+         }
+         
+      }
+   }
+   public void OnMove(InputAction.CallbackContext context)
+   {
+      if(context.phase == InputActionPhase.Performed)
+      {
+         curMovementInput = context.ReadValue<Vector2>();
+      }
+      else if (context.phase == InputActionPhase.Canceled)
+      {
+         curMovementInput = Vector2.zero;
+      }
+      
+   }
+
+   private void Move()
+   {
+      Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+      dir *= moveSpeed;
+      dir.y = rb.velocity.y;
+      
+      rb.velocity = dir;
+   }
+
+   void CameraLook()
+   {
+      camCurXRot +=mouseDelta.y * lookSensitivity;
+      camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+      cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+      transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+   }
+
+
+   bool IsGrounded()
+   {
+      if(transform == null) return false;
+      Ray[] rays = new Ray[4]
+      {
+         new Ray(transform.position + (transform.forward * 0.3f) + (transform.up * 0.01f), Vector3.down),
+         new Ray(transform.position + (-transform.forward * 0.5f) + (transform.up * 0.01f), Vector3.down),
+         new Ray(transform.position + (transform.right * 0.5f) + (transform.up * 0.01f), Vector3.down),
+         new Ray(transform.position + (-transform.right * 0.5f) + (transform.up * 0.01f), Vector3.down)
+      };
+
+      for (int i = 0; i < rays.Length; i++)
+      {
+         
+         Debug.DrawRay(rays[i].origin, rays[i].direction * 0.5f, Color.red);
+         if (Physics.Raycast(rays[i], 5f, groundLayerMask))
+         {
+            return true;  
+         }
+      }
+      return false;
+   }
+
+   public void ToggleCursor(bool toggle)
+   {
+      Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
+      canLook = !toggle;
+   }
+}
